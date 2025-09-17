f155d156b3de178cd4fb134d908a09918252b2ab
SebastianObert <obert2878@gmail.com>
2025-09-17 21:42:19 +0700

    Commit No. 3 - Load and Display Cat Image using Glide

--- app/src/main/java/com/example/lab_week_05/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_05/MainActivity.kt
@@ -3,14 +3,16 @@ package com.example.lab_week_05
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
 import android.widget.TextView
+import com.bumptech.glide.Glide
 import com.example.lab_week_05.api.CatApiService
-import com.example.lab_week_05.model.ImageData // Import ImageData
+import com.example.lab_week_05.model.ImageData
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory // Ganti import menjadi Moshi
+import retrofit2.converter.moshi.MoshiConverterFactory
 
 class MainActivity : AppCompatActivity() {
 
@@ -29,6 +31,10 @@ class MainActivity : AppCompatActivity() {
         findViewById(R.id.api_response)
     }
 
+    private val imageResultView: ImageView by lazy {
+        findViewById(R.id.image_result)
+    }
+
     companion object {
         const val MAIN_ACTIVITY = "MAIN_ACTIVITY"
     }
@@ -36,6 +42,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         getCatImageResponse()
     }
 
@@ -43,19 +50,31 @@ class MainActivity : AppCompatActivity() {
         val call = catApiService.searchImages(1, "full")
 
         call.enqueue(object : Callback<List<ImageData>> {
+
             override fun onResponse(call: Call<List<ImageData>>, response: Response<List<ImageData>>) {
                 if (response.isSuccessful) {
-                    val image = response.body()
-                    val firstImage = image?.firstOrNull()?.imageUrl ?: "No URL"
-                    apiResponseView.text = getString(R.string.image_placeholder, firstImage)
+                    val imageList = response.body()
+                    val imageUrl = imageList?.firstOrNull()?.imageUrl
+
+                    if (!imageUrl.isNullOrBlank()) {
+                        apiResponseView.text = getString(R.string.image_placeholder, imageUrl)
+
+                        Glide.with(this@MainActivity)
+                            .load(imageUrl)
+                            .into(imageResultView)
+                    } else {
+                        Log.d(MAIN_ACTIVITY, "Image URL is null or empty")
+                        apiResponseView.text = "Image URL not found"
+                    }
                 } else {
-                    Log.e(MAIN_ACTIVITY, "Failed to get response\n" +
+                    Log.e(MAIN_ACTIVITY, "Failed to get a successful response\n" +
                             response.errorBody()?.string().orEmpty())
                 }
             }
 
             override fun onFailure(call: Call<List<ImageData>>, t: Throwable) {
                 Log.e(MAIN_ACTIVITY, "Failed to get response", t)
+                apiResponseView.text = "Failed to fetch data. Check connection."
             }
         })
     }
